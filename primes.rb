a70b7edb532b41a2d66269d96ed77f6bbef9beff
Leslie Kornes <lesliekornes@gmail.com>
2023-09-24 14:43:17 -0400

    update

--- primes.rb
+++ primes.rb
@@ -11,9 +11,3 @@ def primes(num)
 end
 
 puts primes(86240)
-
-# supreme way
-# require 'prime'
-# def primeFactors(n)
-#   n.prime_division.map {|factor, times| times == 1 ? "(#{factor})" : "(#{factor}**#{times})"}.join
-# end
